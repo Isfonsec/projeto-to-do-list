5db8e1cad86f4cf53adaaf1e09a412ba07975d81
Isfonsec <isaias2fonsec4@gmail.com>
2025-09-15 07:41:48 -0300

    projeto de estudo finalizado

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
-function add() {
-    var res = document.querySelector('div#res')
-    var text = document.querySelector('input#itarefa')
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
 
-    res.innerHTML += `- ${text.value}</br>` 
+function AddTask() {
+    if(inputBox.value === '') {
+        alert("You must write something!");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = '';
+    saveData();
 }
 
-function feito() {
-    
+listContainer.addEventListener("click", function(e) {
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false)
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
 }
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
