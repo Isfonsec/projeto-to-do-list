6f0fa238c889947e729d1f3e9315be58ae93cd66
Isfonsec <isaias2fonsec4@gmail.com>
2025-09-21 08:27:11 -0300

    Treinando com o projeto

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function AddTask() {
         alert("You must write something!")
     } else {
         let li = document.createElement("li")
-        li.innerHTML = inputBox.value
+        li.innerHTML = inputBox.value 
         listContainer.appendChild(li)
         let span = document.createElement("span")
         span.innerHTML = "\u00d7"
@@ -17,21 +17,21 @@ function AddTask() {
 }
 
 listContainer.addEventListener('click', function(e) {
-    if (e.target.tagName === "LI") {
-        e.target.classList.toggle("checked")
+    if(e.target.tagName == "LI") {
+        e.target.classList.toggle('checked')
         saveData()
-    } else if (e.target.tagName === "SPAN") {
+    } else if (e.target.tagName == "SPAN") {
         e.target.parentElement.remove()
         saveData()
     }
 })
 
 function saveData() {
-    localStorage.setItem("data", listContainer.innerHTML)
+    localStorage.setItem('data', listContainer.innerHTML)
 }
 
 function showTask() {
-    listContainer.innerHTML = localStorage.getItem("data")
+    listContainer.innerHTML = localStorage.getItem('data')
 }
 
 showTask()
