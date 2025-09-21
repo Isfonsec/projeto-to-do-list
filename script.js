ad720f1472fa600f40df3773b47c0b72cba01662
Isfonsec <isaias2fonsec4@gmail.com>
2025-09-21 10:05:56 -0300

    Consegui fazer o projeto inteiro do zero sem consulta

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const inputBox = document.getElementById("input-box")
 const listContainer = document.getElementById("list-container")
 
-function AddTask() {
-    if (inputBox.value === '') {
+function addTask() {
+    if (inputBox.value == '') {
         alert("You must write something!")
     } else {
         let li = document.createElement("li")
@@ -12,15 +12,15 @@ function AddTask() {
         span.innerHTML = "\u00d7"
         li.appendChild(span)
     }
-    inputBox.value = ''
+    inputBox = ''
     saveData()
 }
 
 listContainer.addEventListener('click', function(e) {
-    if(e.target.tagName == "LI") {
-        e.target.classList.toggle('checked')
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked")
         saveData()
-    } else if (e.target.tagName == "SPAN") {
+    } else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove()
         saveData()
     }
